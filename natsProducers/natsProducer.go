0950c9a5fc45e79eb5f7f9fc711fe862364f6a9b
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-10-22 19:25:20 +0100

    create clien for nats producers

--- natsProducers/natsProducer.go
+++ natsProducers/natsProducer.go
@@ -19,7 +19,7 @@ func Producer(ctx context.Context) {
 
 	defer nc.Close()
 
-	subject := "filmsChnan"
+	subject := "filmsChan"
 
 	i := 0
 
@@ -42,4 +42,34 @@ func Producer(ctx context.Context) {
 		}
 
 	}
+}
+
+func consumer(ctx context.Context) {
+	nc, err := nats.Connect("localhost:4222")
+
+	if err != nil {
+		log.Fatal("failed to connect to NATS server", err)
+	}
+
+	defer nc.Close()
+
+	fmt.Println("Connected to NATS server on port 4222")
+
+	subject := "filmsChan"
+
+	messages := make(chan *nats.Msg, 1000)
+
+	subscription, err := nc.ChanSubscribe(subject, messages)
+
+	if err != nil {
+		log.Fatal("failed to subscribe to subject", err)
+	}
+
+	defer func() {
+		subscription.Unsubscribe()
+		close(messages)
+	}()
+
+	log.Println("Subscribed to", subject)
+
 }
