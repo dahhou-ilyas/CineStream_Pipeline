c8665c3116c890b0bed399cc0053beb07383fded
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-10-22 20:11:04 +0100

    create process

--- cleaner/processor.go
+++ cleaner/processor.go
@@ -1,1 +1,29 @@
 package cleaner
+
+import "go-films-pipline/model"
+
+type MovieEnriched struct {
+	model.Movie
+	DecadeCategory string `json:"decade_category"`
+
+	GenreCategories []string `json:"genre_categories"`
+
+	DirectorStats Statistics `json:"director_stats"`
+
+	SentimentAnalysis struct {
+		PlotTone     string  `json:"plot_tone"`
+		KeywordScore float64 `json:"keyword_score"`
+	} `json:"sentiment_analysis"`
+
+	Recommendations []string `json:"recommendations"`
+}
+
+type Statistics struct {
+	TotalMovies int `json:"total_movies"`
+
+	AverageRating float64 `json:"average_rating"`
+}
+
+func ProcessMovie(movie model.Movie) MovieEnriched {
+	return MovieEnriched{}
+}
